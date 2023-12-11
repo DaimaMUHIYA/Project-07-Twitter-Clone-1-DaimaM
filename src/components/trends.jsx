0ef376d3285e5e5bcb8deb06cd131c201098a127
DaimaMUHIYA <muhiyabenjamin@gmail.com>
2023-12-11 21:26:00 +0100

    styling the search bar

--- src/components/trends.jsx
+++ src/components/trends.jsx
@@ -1,15 +1,5 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import iconTwitter from "../images/icon twitter.png";
-import homeFill from "../images/Home-Fill.png";
-import explore from "../images/Explore.png";
-import notification from "../images/Notifications.png";
-import messages from "../images/Messages.png";
-import bookmarks from "../images/Bookmarks.png";
-import lists from "../images/Lists.png";
-import profile from "../images/Profile.png";
-import more from "../images/More.png";
-import tweetBtnXl from "../images/Tweet-Buton-Xl.png";
 import searchIcon from "../images/Search.svg";
 
 const Trends = () => {
