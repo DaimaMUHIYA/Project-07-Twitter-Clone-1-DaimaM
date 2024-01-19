ee54351e65bd642bd9972632ac5de215b1f05b6c
DaimaMUHIYA <muhiyabenjamin@gmail.com>
2024-01-19 09:46:07 +0100

    convert avatar image into a link

--- src/components/tweet-editor.jsx
+++ src/components/tweet-editor.jsx
@@ -1,28 +1,29 @@
-import React from 'react';
-import meta from '../images/Meta.png';
+import React from "react";
+import meta from "../images/Meta.png";
+import { Link } from "react-router-dom";
 
 // #3. tweet-editor.
 
 function TweetEditor() {
   return (
+    <div className="tweet-editor">
+      <Link>
+        <img className="avatar" src="src/images/profile-photo.png" alt="Logo" />
+      </Link>
 
-    <div className ='tweet-editor'>
-      <img className='avatar' src ="src/images/profile-photo.png" alt ="Logo"/>
-      
-      <div className='tweet-editor-form'>
-        <input className='tweet-editor-input' type="text" placeholder="What's happening ?"/>
+      <div className="tweet-editor-form">
+        <input
+          className="tweet-editor-input"
+          type="text"
+          placeholder="What's happening ?"
+        />
 
-        <div className='tweet-editor-buttons'>
-          <img className='tweet-editor-actions' src={meta} alt="Logo" />
-          <button className='button'>Tweet</button>
+        <div className="tweet-editor-buttons">
+          <img className="tweet-editor-actions" src={meta} alt="Logo" />
+          <button className="button">Tweet</button>
         </div>
       </div>
-      
     </div>
-    
-
-    
-    
   );
 }
 
